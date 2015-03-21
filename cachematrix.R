29ad02b46bbf01d368354a95ec3bc619b4c5555e
falserino <james.herring@outlook.com>
2015-03-20 23:56:16 -0400

    CacheMatrix.R Changes with samples at bottom
    basically duplicated Functions from sample script and added matrix
    object.  Note: This requires a square matrix as was directed to use
    solver function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,77 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## This function creates a special "matrix" object
+## that can cache its inverse.
 
 
-## Write a short comment describing this function
+        makeCacheMatrix <- function(x = matrix()) {
+                m <- NULL
+                ## sets the value of the matrix(x)
+                set <-  function(y){
+                        ## This sets the Global Varaibles using the <<- operator
+                        x  <<- y
+                        m <<- NULL
+                }
+                ##retrieve the function value of matrix(x)
+                get <- function() x
+                ##Sets the value of solve function
+                setmatrix <- function(solve) m <<- solve
+                ## retrieves the value of solve
+                getmatrix <- function() m
+                list(set = set, get = get
+                     ,setmatrix = setmatrix
+                     ,getmatrix = getmatrix)
+        }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
+
+
+        cacheSolve <- function(x = matrix(), ...) {
+                ## checks the value of m in cache - if empty it runs function matrix(x)
+                m <- x$getmatrix()
+                if(!is.null(m)){
+                        message("getting cached data")
+                        return(m)
+                }
+                data <- x$get()
+                ## using the first position of solve for a square matrix inversion as b is empty
+                m <- solve(data, ...)
+                x$setmatrix(m)
+                m
+        }
+        
+        
+        ##sample set inversion should be -5, 4, 4.5, -3.5        
+        ## a <- matrix(c(7,8,9,10), 2)
+        ## b <- makeCacheMatrix(a)
+        ## cacheSolve(b)
+        
+
+        
+        ## creating a regular normal distribution matrix 100x100
+        ## set.seed(125)
+        ## a <- matrix(rnorm(100), 10,10)
+        ## b <- makeCacheMatrix(a)
+        ## cacheSolve(b)
+        
+        ##A group took a trip on a bus, at $3 per child and $3.20 per adult for a total of $118.40. 
+        ##They took the train back at $3.50 per child and $3.60 per adult for a total of $135.20.
+        ##How many children, and how many adults?
+        ## a <- matrix(c(3, 3.2, 3.5, 3.6), 2,2)
+        ## b <- makeCacheMatrix(a)
+        ## cacheSolve(b)
+        
+        ## solver equation (NOTE: OUT OF SCOPE FOR ASSIGNMENT)
+        ## inverse category 1 "Children" = (-9,8)
+        ## (118.4 x -9 + 135.2 x 8) = 16 Children
+        
+        ## inverse Category 2 "Adults" = (8.75, -7.50)
+        ## (118.4 x 8.75 + 135.2 x -7.50) = 22 Adults
+        
+
